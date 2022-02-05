ede59897050e4289d9017ca39fd95d6d0a7f273f
andersh4 <anders.hunt367@gmail.com>
2022-02-04 18:32:59 -0800

    Add files via upload

--- WebApp/test.py
+++ WebApp/test.py
@@ -8,19 +8,24 @@ app = Flask(__name__)
 
 color_array1 = []
 
+
+
 @app.route('/array',methods=['POST'])
 def load_array():
 	array_json = json.loads(request.get_json())
 	print(array_json)	
-	color_array1 = array_json
+	#color_array1 = array_json
+	with open("test.txt", "w") as fo:
+		fo.write(json.dumps(array_json))
 	return array_json
 
 @app.route('/')
 def index():
-   return render_template('index.html', color_array=color_array1)
+   with open("test.txt", "r") as fo:
+      color_array1 = json.loads(fo.read())
 
-
-
+   print(color_array1["array"][0])
+   return render_template('index.html', color_array=color_array1["array"])
 
 
 if __name__ == '__main__':
